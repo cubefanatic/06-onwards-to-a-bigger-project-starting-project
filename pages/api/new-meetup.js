91d1b4b9a04239cb18cf7f6e8fb6d7ccaeb02709
Farhan Sayed <farhanthegamer162@gmail.com>
2022-02-16 20:46:22 +0400

    sending http requests

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,12 +5,12 @@ async function handler(req, res) {
     if(req.method==='POST'){
         const data = req.body;
 
-        const client = await MongoClient.connect('mongodb+srv://admin-farhan:YGVrmoP8LqgW7Mw5@cluster0.iddhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+        const client = await MongoClient.connect('mongodb+srv://admin-farhan:1234@cluster0.iddhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
         const db = client.db();
 
         const meetupCollection = db.collection('meetups');
 
-        const result = await meetupsCollection.insertOne({data});
+        const result = await meetupCollection.insertOne({data});
         console.log(result);
         client.close();
         res.status(201).json({ message: 'Meetup inserted!'});
