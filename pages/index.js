04faef5ee776c7cc6c2a36f6097c0310d5aa73c1
Farhan Sayed <farhanthegamer162@gmail.com>
2022-02-17 19:45:15 +0400

    Getting data from the database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
 const DUMMY_MEETUPS = [
     {
@@ -35,12 +36,28 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
-  return{
+
+  const client = await MongoClient.connect('mongodb+srv://admin-farhan:1234@cluster0.iddhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+  const db = client.db();
+
+  const meetupCollection = db.collection('meetups');
+
+  const meetups = await meetupCollection.find().toArray();
+
+  client.close();
+
+  return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 1
+    revalidate: 1,
   };
+
 }
 
 export default HomePage;
